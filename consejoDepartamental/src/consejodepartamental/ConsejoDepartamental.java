cd98b6b9b266e1a9ab0cd647701156fd499e47ef
Diego Barture <diego@latamready.com>
2023-05-30 02:49:32 -0500

    30/05/2023 - Diego Barturen

---
+++ consejoDepartamental/src/consejodepartamental/ConsejoDepartamental.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package consejodepartamental;
+
+/**
+ *
+ * @author Diego
+ */
+public class ConsejoDepartamental {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+}
